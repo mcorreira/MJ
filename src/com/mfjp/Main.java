b09cac1e786f06065a8994152a33604d429b1d61
mcorreira <megancorreiradev@gmail.com>
2025-08-30 17:38:01 -0400

    first commit

---
+++ src/com/mfjp/Main.java
@@ -0,0 +1,8 @@
+package com.mfjp; // package statement terminated by ;
+
+public class Main { // main class
+
+    public static void main(String [] args) { // main method accessible by other parts of the program, static is important, will not return anything
+        System.out.println("Hello World"); //executing print line method, surround textual data with ""
+    }
+}
