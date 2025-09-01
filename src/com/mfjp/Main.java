b7ffdcec79fbc530de0b99bb881607e04040dc19
mcorreira <megancorreiradev@gmail.com>
2025-09-01 17:20:29 -0400

    variables, constants, types

--- src/com/mfjp/Main.java
+++ src/com/mfjp/Main.java
@@ -1,8 +1,27 @@
 package com.mfjp; // package statement terminated by ;
+import java.util.Arrays;
+import java.util.Date;
+
 
 public class Main { // main class
 
     public static void main(String [] args) { // main method accessible by other parts of the program, static is important, will not return anything
-        System.out.println("Hello World"); //executing print line method, surround textual data with ""
+        byte age = 30;
+        long viewsCount = 3_124_456_789L; //need L so it is recognized as LONG
+        float price = 10.99F; //need F so it is recognized as FLOAT, otherwise it thinks it is double
+        char letter = 'A'; //surround single character with single quotes and multiple characters with double quotes
+        boolean isEligible = false;
+        byte x = 1;
+        byte y = x;
+        int[] numbers = {2, 3, 5, 1, 4};
+        Arrays.sort(numbers);
+
+
+        String message = "Hello World!" + "!!";
+
+        //Reference Types
+        Date now = new Date();
+        now.getTime();
+        System.out.println(Arrays.toString(numbers)); //executing print line method, surround textual data with ""
     }
 }
