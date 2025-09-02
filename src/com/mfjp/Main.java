22be343b59c73a56749cb9185c3bef2fb4b90cb0
mcorreira <megancorreiradev@gmail.com>
2025-09-01 23:04:30 -0400

    mortgage calculator

--- src/com/mfjp/Main.java
+++ src/com/mfjp/Main.java
@@ -1,26 +1,34 @@
 package com.mfjp; // package statement terminated by ;
 import java.util.Arrays;
-import java.util.Date;
+
+import java.util.Scanner;
+
 
 
 public class Main { // main class
 
     public static void main(String [] args) { // main method accessible by other parts of the program, static is important, will not return anything
-        byte age = 30;
-        long viewsCount = 3_124_456_789L; //need L so it is recognized as LONG
-        float price = 10.99F; //need F so it is recognized as FLOAT, otherwise it thinks it is double
-        char letter = 'A'; //surround single character with single quotes and multiple characters with double quotes
-        boolean isEligible = false;
-        byte x = 1;
-        byte y = x;
-        int[] numbers = {2, 3, 5, 1, 4};
-        Arrays.sort(numbers);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Principal:     ");
+        double principal = scanner.nextDouble();
+        System.out.println("Principal: " + principal);
 
-        int [][] numberMat = new int[2][3];
-        numberMat[0][0] = 1;
-        System.out.println(Arrays.deepToString(numberMat));
+p
+        //byte age = 30;
+        //long viewsCount = 3_124_456_789L; //need L so it is recognized as LONG
+        //float price = 10.99F; //need F so it is recognized as FLOAT, otherwise it thinks it is double
+       // char letter = 'A'; //surround single character with single quotes and multiple characters with double quotes
+        //boolean isEligible = false;
+        //byte x = 1;
+        //byte y = x;
+        //int[] numbers = {2, 3, 5, 1, 4};
+        //Arrays.sort(numbers);
 
-        String message = "Hello World!" + "!!";
+        //int [][] numberMat = new int[2][3];
+        //numberMat[0][0] = 1;
+       // System.out.println(Arrays.deepToString(numberMat));
+
+        //String message = "Hello World!" + "!!";
 
         //Reference Types
        // Date now = new Date();
